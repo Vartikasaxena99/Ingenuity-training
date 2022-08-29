7475e08e64b988471ce4a482ce78d72b97eba89c
Vartika Saxena <vartikasaxena251@gmail.com>
2022-08-29 17:56:22 +0530

    done

--- practical15/script.js
+++ practical15/script.js
@@ -1,56 +1,153 @@
-//selectors
-var canvas=document.getElementById("canvas");
-// const ctx=canvas.getContext("2d");
-var img = this[0]; // grab the first one, shake the jquery
-var imgWidth = this.width();
-var imgHeight = this.height();
+// selector
+var canvas = document.getElementById("canvas");
+// var ctx = canvas.getContext("2d");
+var ctx2=canvas.getContext("2d");
+const image = document.getElementById("source");
+// ctx2.beginPath();
+//  ctx2.rect(300,50,150,100);
+//  ctx2.fillStyle="blue";
+//  ctx2.fill();
 
-var canvas = $("<canvas>");
 
-// set the acutal w and h
-canvas[0].width = imgWidth;
-canvas[0].height = imgHeight;
+var circle={
+    x:200,
+    y:200,
+    size:30,
+    dx:5,
+    dy:5,
+}
+//Draw the circle
 
-var ctx = canvas[0].getContext('2d');
-console.log(ctx);
-ctx.drawImage(img,0,0, imgWidth, imgHeight);
+function drawCircle()
+{
+    ctx2.beginPath();
+    ctx2.arc(circle.x,circle.y,circle.size,0,Math.PI*2);
+    ctx2.fillStyle="purple";
+    ctx2.fill();
+}
+drawCircle();
+function update2()
+{
+    ctx2.clearRect(0,0,canvas.width,canvas.height);
+    drawCircle();
+    circle.x+=-circle.dx;
+    circle.y+=circle.dy;
+    //detect ball
+    if(circle.x+circle.size > canvas.width ||circle.x-circle.size <0 )
+    {
+        circle.dx*=-1;
+    }
+    if(circle.y+circle.size > canvas.height ||circle.y-circle.size <0 )
+    {
+        circle.dy*=-1;
+    }
+    requestAnimationFrame(update2);
+    console.log(123);
+}
+update2();
+const player = {
+  w: 50,
+  h: 70,
+  x: 20,
+  y: 200,
+  speed: 5,
+  dx: 0,
+  dy: 0,
+};
 
-var pixels = ctx.getImageData(0,0, imgWidth, imgHeight);
-var data = pixels.data;
-//Animation
-// var circle={
-//     x:200,
-//     y:200,
-//     size:30,
-//     dx:5,
-//     dy:5,
-// }
-// //Draw the circle
+function drawPlayer() {
+  ctx.drawImage(image, player.x, player.y, player.w, player.h);
+}
 
-// function drawCircle()
-// {
-//     ctx.beginPath();
-//     ctx.arc(circle.x,circle.y,circle.size,0,Math.PI*2);
-//     ctx.fillStyle="purple";
-//     ctx.fill();
-// }
-// drawCircle();
-// function update()
-// {
-//     ctx.clearRect(0,0,canvas.width,canvas.height);
-//     drawCircle();
-//     circle.x+=-circle.dx;
-//     // circle.y+=circle.dy;
-//     //detect ball
-//     if(circle.x+circle.size > canvas.width ||circle.x-circle.size <0 )
-//     {
-//         circle.dx*=-1;
-//     }
-//     // if(circle.y+circle.size > canvas.height ||circle.y-circle.size <0 )
-//     // {
-//     //     circle.dy*=-1;
-//     // }
-//     requestAnimationFrame(update);
-//     console.log(123);
-// }
-// update();
+function clear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function newPos() {
+  player.x += player.dx;
+  player.y += player.dy;
+
+  detectWalls();
+}
+
+function detectWalls() {
+  // Left wall
+  if (player.x < 0) {
+    player.x = 0;
+  }
+
+  // Right Wall
+  if (player.x + player.w > canvas.width) {
+    player.x = canvas.width - player.w;
+  }
+
+  // Top wall
+  if (player.y < 0) {
+    player.y = 0;
+  }
+
+  // Bottom Wall
+  if (player.y + player.h > canvas.height) {
+    player.y = canvas.height - player.h;
+  }
+}
+// Create Animation
+function update() {
+  clear();
+
+  drawPlayer();
+
+  newPos();
+
+  requestAnimationFrame(update);
+}
+
+function moveUp() {
+  player.dy = -player.speed;
+}
+
+function moveDown() {
+  player.dy = player.speed;
+}
+
+function moveRight() {
+  player.dx = player.speed;
+}
+
+function moveLeft() {
+  player.dx = -player.speed;
+}
+
+function keyDown(e) {
+  console.log(e.key);
+  if (e.key === "ArrowRight" || e.key === "Right") {
+    moveRight();
+  } else if (e.key === "ArrowLeft" || e.key === "Left") {
+    moveLeft();
+  } else if (e.key === "ArrowUp" || e.key === "Up") {
+    moveUp();
+  } else if (e.key === "ArrowDown" || e.key === "Down") {
+    moveDown();
+  }
+}
+
+function keyUp(e) {
+  if (
+    e.key == "Right" ||
+    e.key == "ArrowRight" ||
+    e.key == "Left" ||
+    e.key == "ArrowLeft" ||
+    e.key == "Up" ||
+    e.key == "ArrowUp" ||
+    e.key == "Down" ||
+    e.key == "ArrowDown"
+  ) {
+    player.dx = 0;
+    player.dy = 0;
+  }
+}
+
+update();
+
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
